d1ad4be472ebce1154626c930e3f2a81b47bdb1d
Zz_muggle <407638319@qq.com>
2024-10-18 16:49:10 +0800

    test.

--- src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
+++ src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -36,11 +36,12 @@ public class RefreshTokenInterceptor implements HandlerInterceptor {
         //6. 放行。*/
 
         //1. 获取请求头中的token。
+//        System.out.println("6666");
         String token = request.getHeader("authorization");
 
         if(StrUtil.isBlank(token)){
-//            response.setStatus(401);
-            return true;
+            response.setStatus(401);
+            return false;
         }
 
         //2. 基于token获取redis中的用户。
@@ -50,9 +51,9 @@ public class RefreshTokenInterceptor implements HandlerInterceptor {
         //3. 判断用户是否存在。
         if(userMap.isEmpty()){
 //            //4. 不存在，拦截。
-//            response.setStatus(401);
-//            return false;
-            return true;
+            response.setStatus(401);
+            return false;
+//            return true;
 
         }
 
@@ -72,6 +73,7 @@ public class RefreshTokenInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+//        System.out.println("999");
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
